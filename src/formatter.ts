7e57197a223d363dc39068ea1b7aa20397074af0
rmcmk <ryley.kimmel@live.com>
2023-11-20 21:29:30 -0500

    chore: deal with extra escapes in regex, improve errs

--- src/formatter.ts
+++ src/formatter.ts
@@ -2,17 +2,22 @@ import { Foundry } from "./foundry";
 import * as vscode from "vscode";
 import { executeCommand } from "./bin";
 
+class ParseError extends Error {
+  constructor(message: string) {
+    super(message);
+
+    // Set the prototype explicitly.
+    Object.setPrototypeOf(this, ParseError.prototype);
+  }
+}
+
 const EMPTY_TEXT_EDITS: vscode.TextEdit[] = [];
 
 export async function parseAndFormat(
   foundry: Foundry,
   document: vscode.TextDocument
 ): Promise<vscode.TextEdit[]> {
-  const textEdits = await _parseAndFormat(foundry, document);
-  if (textEdits.length === 0) {
-    vscode.window.showInformationMessage("No formatting changes detected.");
-  }
-  return textEdits;
+  return await _parseAndFormat(foundry, document);
 }
 async function _parseAndFormat(
   foundry: Foundry,
@@ -23,26 +28,37 @@ async function _parseAndFormat(
     const cmd = `"${foundry.forgeBinaryPath}" fmt "${path}" --check`;
     const { stdout, exitCode } = await executeCommand(cmd);
 
+    /**
+     * Exit codes:
+     * 0: No diffs found, no formatting required.
+     * 1: Formatting required.
+     * @see https://github.com/foundry-rs/foundry/blob/master/crates/forge/bin/cmd/fmt.rs#L34
+     */
     if (exitCode === 0) {
       return EMPTY_TEXT_EDITS;
     }
 
     const edits = parseDiff(stdout, document);
     if (edits.length === 0) {
-      return EMPTY_TEXT_EDITS;
+      throw new ParseError(
+        "Parsing failed. Forge returned an exit code of 1 but no diffs were found."
+      );
     }
 
     console.log(`Diffs: ${edits.length} in ${path}`);
-
     return edits;
   } catch (error) {
     console.error("Formatting failed with error: ", error);
-    vscode.window.showErrorMessage(
-      "Formatting failed. Please check the formatter settings and path. See the console for more details."
-    );
-  }
 
-  return EMPTY_TEXT_EDITS;
+    let message =
+      "Formatting failed. Check `Extension Host` logs in VSCode for more details.";
+    if (error instanceof ParseError) {
+      message = error.message;
+    }
+
+    vscode.window.showErrorMessage(message);
+    throw error; // Rethrow for caller
+  }
 }
 
 function parseDiff(
@@ -60,9 +76,9 @@ function parseDiff(
   // Group 2: End line number (optional)
   // Group 3: Action (+, -, or nothing)
   // Group 4: The rest of the line
-  const regex = /^(\d+)?\s*(\d+)?\s*\|\s*([-+])?(.*)$/;
+  const regex = /^(\d+)?\s*(\d+)?\s*\|\s*([-+])?\s*(.*)\s*$/;
 
-  loop: for (const line of lines) {
+  for (const line of lines) {
     if (line.startsWith(diffStartToken)) {
       currentFile = line.slice(diffStartToken.length).trim();
       console.log(`Parsing diff for file: ${currentFile}`);
@@ -70,10 +86,7 @@ function parseDiff(
       const match = line.match(regex);
 
       if (!match) {
-        vscode.window.showErrorMessage(
-          `Unable to format: Unexpected token on line: ${line}`
-        );
-        break;
+        throw new ParseError(`Unable to parse diff line: ${line}`);
       }
 
       const oldLineNumber = parseInt(match[1]);
@@ -81,28 +94,22 @@ function parseDiff(
       const action = match[3];
       const content = match[4];
 
-      switch (action) {
-        case "+":
-          if (!isNaN(oldLineNumber) || isNaN(newLineNumber)) {
-            vscode.window.showErrorMessage(
-              `Unable to format: New line number is missing: ${line}`
-            );
-            break loop;
-          }
-          const position = new vscode.Position(newLineNumber - 1, 0);
-          textEdits.push(vscode.TextEdit.insert(position, content));
-          continue loop;
+      if (action === "+") {
+        if (!isNaN(oldLineNumber) || isNaN(newLineNumber)) {
+          throw new ParseError(`Unable to parse diff line: ${line}`);
+        }
+        const position = new vscode.Position(newLineNumber - 1, 0);
+        textEdits.push(vscode.TextEdit.insert(position, content));
+        continue;
+      }
 
-        case "-":
-          if (isNaN(oldLineNumber) || !isNaN(newLineNumber)) {
-            vscode.window.showErrorMessage(
-              `Unable to format: Old line number is missing: ${line}`
-            );
-            break loop;
-          }
-          const range = document.lineAt(oldLineNumber - 1).range;
-          textEdits.push(vscode.TextEdit.delete(range));
-          continue loop;
+      if (action === "-") {
+        if (isNaN(oldLineNumber) || !isNaN(newLineNumber)) {
+          throw new ParseError(`Unable to parse diff line: ${line}`);
+        }
+        const range = document.lineAt(oldLineNumber - 1).range;
+        textEdits.push(vscode.TextEdit.delete(range));
+        continue;
       }
     }
   }
