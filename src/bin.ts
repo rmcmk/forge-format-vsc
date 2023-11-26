f915773d8e1c100c794209988f8a4acad1759cd8
rmcmk <ryley.kimmel@live.com>
2023-11-25 19:10:05 -0500

    chore: better process spawning, take into account more state

--- src/bin.ts
+++ src/bin.ts
@@ -26,25 +26,35 @@ export function executeCommand(
   if (cwd) {
     console.log(`Working directory: ${cwd}`);
   }
-  if (input) { // TODO: debug mode & verbosity settings for these kinds of logs
+  if (input) {
+    // TODO: debug mode & verbosity settings for these kinds of logs
     console.log(`Input:\n${input}`);
   }
 
   return new Promise((resolve, reject) => {
     const childProcess = spawnCommand(command, args, cwd);
 
+    if (!childProcess) {
+      reject(new Error("Failed to spawn child process"));
+      return;
+    }
+
     let stdoutData = "";
     let stderrData = "";
 
     // Capture stdout
-    childProcess.stdout.on("data", (data) => {
-      stdoutData += data.toString();
-    });
+    if (childProcess.stdout) {
+      childProcess.stdout.on("data", (data) => {
+        stdoutData += data.toString();
+      });
+    }
 
     // Capture stderr
-    childProcess.stderr.on("data", (data) => {
-      stderrData += data.toString();
-    });
+    if (childProcess.stderr) {
+      childProcess.stderr.on("data", (data) => {
+        stderrData += data.toString();
+      });
+    }
 
     // Handle process errors
     childProcess.on("error", (error) => {
@@ -52,24 +62,34 @@ export function executeCommand(
     });
 
     // Process has closed
-    childProcess.on("close", (exitCode) => {
-      // Check for errors based on exit code and stderr content
-      if (exitCode !== 0 && stderrData.length > 0) {
-        reject(stderrData);
-        // TODO: In this case, stdout has an informative error message. We should consider also logging this somewhere.
-      } else {
-        resolve({ exitCode, stdout: stdoutData });
+    childProcess.on(
+      "close",
+      (code: number | null, signal: NodeJS.Signals | null) => {
+        if (code !== null) {
+          // Check for errors based on exit code and stderr content
+          if (code !== 0 && stderrData.length > 0) {
+            reject(stderrData);
+            // TODO: In this case, stdout has an informative error message. We should consider also logging this somewhere.
+          } else {
+            resolve({ exitCode: code, stdout: stdoutData });
+          }
+        } else if (signal !== null) {
+          throw new Error(`Unexpected process termination. Signal: ${signal}`);
+        } else {
+          throw new Error(
+            `Unexpected process termination. Null code and signal.`
+          );
+        }
       }
-    });
+    );
 
     // Write input to stdin if provided
-    if (input) {
+    if (input && childProcess.stdin) {
       childProcess.stdin.write(input);
       childProcess.stdin.end();
     }
   });
 }
-
 /**
  * Spawns a child process with specified command and arguments.
  * @param command The command to execute.
@@ -81,9 +101,14 @@ function spawnCommand(
   command: string,
   args: string[],
   cwd?: string
-): ChildProcess {
-  return spawn(command, args, {
-    stdio: ["pipe", "pipe", "pipe"],
-    cwd: cwd,
-  });
+): ChildProcess | undefined {
+  try {
+    return spawn(command, args, {
+      stdio: ["pipe", "pipe", "pipe"],
+      cwd: cwd,
+    });
+  } catch (error) {
+    console.error("Error spawning command:", error);
+    return undefined;
+  }
 }
